84d111f74c24646c7077eb8eee09f117101850d3
Luuan11 <luancraft55@gmail.com>
2024-04-07 19:27:42 -0300

    :sparkles: feat: metodos on go

--- 15 - Metodos/metodos.go
+++ 15 - Metodos/metodos.go
@@ -8,11 +8,31 @@ type user struct {
 }
 
 func (u user) salvar() {
-	fmt.Printf("Salvando esse user: %s no banco da idade %d", u.nome, u.idade)
+	fmt.Printf("Salvando esse user %s da idade %d no banco de dados de usuarios \n", u.nome, u.idade)
+}
+
+func (u user) verificaIdade(){
+	if u.idade >= 18{
+		fmt.Printf("informação extra, o user %s é maior de idade \n", u.nome)
+	} else {
+		fmt.Printf("que pena, o user %s é menor de idade \n", u.nome)
+	}
 }
 
 func main() {
 	user1 := user{"Luan", 21}
-	fmt.Println(user1)
+	
+	user2 := user{
+		nome: "Giovana",
+		idade: 22,
+	}
+
+	user3 := user{"Alvaro", 17}
+	
 	user1.salvar()
+	user2.salvar()
+	user3.salvar()
+	user1.verificaIdade()
+	user2.verificaIdade()
+	user3.verificaIdade()
 }
